4a4344d950c633805475f59ed4a3f4ee400f6666
danieldannitz <danielgnlmenezes@gmail.com>
2022-07-14 23:23:26 -0300

    First Component

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,13 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="#">Acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
