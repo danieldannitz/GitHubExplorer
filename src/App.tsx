78733285698c7c380c01b5cb66c090025ee3b079
danieldannitz <danielgnlmenezes@gmail.com>
2022-07-14 23:26:17 -0300

    include RepositoryList

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { RepositoryList } from "./components/RepositoryList";
 import "./styles/global.scss";
 
 export function App() {
   return (
     <div>
-      <h1>Hello, world</h1>
+      <RepositoryList />
     </div>
   );
 }
