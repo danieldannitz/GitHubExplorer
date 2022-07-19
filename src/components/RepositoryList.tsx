e1c90f4c87ffc5ddf249205d9d0f37d3c0089876
danieldannitz <danielgnlmenezes@gmail.com>
2022-07-19 11:11:33 -0300

    search repository textarea

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import "../styles/respositories.scss";
@@ -7,22 +7,37 @@ import "../styles/respositories.scss";
 
 export function RepositoryList() {
   const [repositories, setRepositories] = useState<any[]>([]);
-
+  const [searchTerm, setSearchTerm] = useState();
   useEffect(() => {
     fetch("https://api.github.com/users/danieldannitz/repos")
       .then((response) => response.json())
       .then((data) => setRepositories(data));
   }, []);
 
+  function handleFilter() {
+    setSearchTerm(event?.target.value);
+  }
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
+      <textarea value={searchTerm} onChange={handleFilter}></textarea>
       <ul>
-        {repositories.map((repository) => {
-          return (
-            <RepositoryItem key={repository.name} repository={repository} />
-          );
-        })}
+        {repositories
+          .filter((val) => {
+            if (searchTerm === "") {
+              return val;
+            } else if (
+              val.name.toLowerCase().includes(searchTerm?.toLowerCase())
+            ) {
+              return val;
+            }
+          })
+          .map((repository) => {
+            return (
+              <RepositoryItem key={repository.name} repository={repository} />
+            );
+          })}
       </ul>
     </section>
   );
